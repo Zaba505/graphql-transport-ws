9adcf2e19352774fa194c5d33069e3bcb1f03224
Zaba505 <cakub6@gmx.com>
2020-06-16 00:59:40 -0400

    test(e2e): add coverage for basic subscription

--- e2e_test.go
+++ e2e_test.go
@@ -52,6 +52,50 @@ func TestE2E(t *testing.T) {
 	}
 }
 
+func TestE2E_Subscription(t *testing.T) {
+	srv := httptest.NewServer(NewHandler(HandlerFunc(testHandler)))
+	defer srv.Close()
+
+	conn, err := Dial(context.Background(), "ws://"+srv.Listener.Addr().String())
+	if err != nil {
+		t.Errorf("unexpected error when dialing: %s", err)
+		return
+	}
+	defer conn.Close()
+
+	client := NewClient(conn)
+	sub, err := client.Subscribe(context.Background(), &Request{Query: "{ hello { world } }"})
+	if err != nil {
+		t.Errorf("unexpected error when subscribing: %s", err)
+		return
+	}
+	defer sub.Unsubscribe()
+
+	resp, err := sub.Recv(context.TODO())
+	if err != nil {
+		t.Error("unexpected error when receiving response", err)
+		return
+	}
+
+	var testResp struct {
+		Hello struct {
+			World string
+		}
+	}
+	err = json.Unmarshal(resp.Data, &testResp)
+	if err != nil {
+		t.Logf("response data: %s", string(resp.Data))
+		t.Errorf("unexpected error when unmarshalling response: %s", err)
+		return
+	}
+
+	if testResp.Hello.World != "this is a test" {
+		t.Logf("expected: %s, but got: %s", "this is a test", testResp.Hello.World)
+		t.Fail()
+		return
+	}
+}
+
 func TestConcurrency(t *testing.T) {
 	srv := httptest.NewServer(NewHandler(HandlerFunc(testHandler)))
 	defer srv.Close()
