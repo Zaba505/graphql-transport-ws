9c66097ebc9816dfcbde4907fc193be6a46e2299
Zaba505 <cakub6@gmx.com>
2020-06-18 23:36:25 -0400

    test(payload): add cases for json marshaling

--- payload_test.go
+++ payload_test.go
@@ -3,6 +3,7 @@ package gws
 import (
 	"bytes"
 	"encoding/json"
+	"reflect"
 	"testing"
 )
 
@@ -97,9 +98,64 @@ func TestOpMessage_MarshalJSON(t *testing.T) {
 		Ex   string
 	}{
 		{
+			Name: "Missing Payload",
+			Msg:  operationMessage{ID: "1", Type: gqlComplete},
+			Ex:   `{"id":"1","type":"complete"}`,
+		},
+		{
+			Name: "Only Type",
+			Msg:  operationMessage{Type: gqlConnectionInit},
+			Ex:   `{"type":"connection_init"}`,
+		},
+		{
+			Name: "Just Query",
+			Msg:  operationMessage{ID: "1", Type: gqlStart, Payload: &Request{Query: "{ hello { world } }"}},
+			Ex:   `{"id":"1","type":"start","payload":{"query":"{ hello { world } }"}}`,
+		},
+		{
+			Name: "With Variables",
+			Msg:  operationMessage{ID: "1", Type: gqlStart, Payload: &Request{Query: "{ hello { world } }", Variables: map[string]interface{}{"hello": "world"}}},
+			Ex:   `{"id":"1","type":"start","payload":{"query":"{ hello { world } }","variables":{"hello":"world"}}}`,
+		},
+		{
+			Name: "Full Request",
+			Msg:  operationMessage{ID: "1", Type: gqlStart, Payload: &Request{Query: "{ hello { world } }", Variables: map[string]interface{}{"hello": "world"}, OperationName: "Test"}},
+			Ex:   `{"id":"1","type":"start","payload":{"query":"{ hello { world } }","variables":{"hello":"world"},"operationName":"Test"}}`,
+		},
+		{
+			Name: "With Errors",
+			Msg:  operationMessage{ID: "1", Type: gqlData, Payload: &Response{Data: []byte(`{"hello":{"world":"test"}}`), Errors: []json.RawMessage{[]byte(`{"msg":"test"}`), []byte(`{"msg":"test"}`)}}},
+			Ex:   `{"id":"1","type":"data","payload":{"data":{"hello":{"world":"test"}},"errors":[{"msg":"test"},{"msg":"test"}]}}`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(subT *testing.T) {
+			b, err := json.Marshal(&testCase.Msg)
+			if err != nil {
+				subT.Error(err)
+				return
+			}
+
+			if !bytes.Equal(b, []byte(testCase.Ex)) {
+				subT.Logf("expected: %s, got: %s", string(testCase.Ex), string(b))
+				subT.Fail()
+				return
+			}
+		})
+	}
+}
+
+func TestOpMessage_RoundTrip(t *testing.T) {
+	testCases := []struct {
+		Name string
+		Msg  operationMessage
+		Ex   string
+	}{
+		{
 			Name: "All Fields",
-			Msg:  operationMessage{ID: "1", Type: gqlData, Payload: &Request{Query: "{ hello { world } }"}},
-			Ex:   `{"id":"1","type":"data","payload":{"query":"{ hello { world } }"}}`,
+			Msg:  operationMessage{ID: "1", Type: gqlStart, Payload: &Request{Query: "{ hello { world } }"}},
+			Ex:   `{"id":"1","type":"start","payload":{"query":"{ hello { world } }"}}`,
 		},
 		{
 			Name: "Missing Payload",
@@ -115,13 +171,23 @@ func TestOpMessage_MarshalJSON(t *testing.T) {
 
 	for _, testCase := range testCases {
 		t.Run(testCase.Name, func(subT *testing.T) {
+			subT.Parallel()
+
 			b, err := json.Marshal(&testCase.Msg)
 			if err != nil {
 				subT.Error(err)
 				return
 			}
 
-			if !bytes.Equal(b, []byte(testCase.Ex)) {
+			var msg operationMessage
+			err = json.Unmarshal(b, &msg)
+			if err != nil {
+				subT.Error(err)
+				return
+			}
+
+			if !reflect.DeepEqual(msg, testCase.Msg) {
+				subT.Logf("expected: %#v, got: %#v", testCase.Msg, msg)
 				subT.Fail()
 				return
 			}
@@ -137,7 +203,7 @@ const benchReq = `{
   }
 }`
 
-func BenchmarkOpMessage_Unmarshal(b *testing.B) {
+func BenchmarkOpMessage_UnmarshalJSON(b *testing.B) {
 	b.Run("Via UnmarshalJSON", func(subB *testing.B) {
 		for i := 0; i < subB.N; i++ {
 			msg := new(operationMessage)
